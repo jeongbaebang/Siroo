e1e4cd19d4142c3e44e8ed2048b4d20c28545b5f
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2024-01-14 14:50:59 +0900

    Initial Commit

---
+++ Siroo/SirooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SirooApp.swift
+//  Siroo
+//
+//  Created by jeongbae bang on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SirooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
