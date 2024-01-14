2442e89abc797e136d35977b772fb9a4ee77c135
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2024-01-14 16:20:38 +0900

    add: 페이지 네비게이션 & 로고 추가

---
+++ Siroo/Views/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Siroo
+//
+//  Created by jeongbae bang on 1/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            TaskView()
+                .tabItem {
+                    Label("작업", systemImage: "timer")
+                }
+            TaskStatusView()
+                .tabItem {
+                    Label("작업현황", systemImage: "chart.bar.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Siroo/Views/TaskView.swift
@@ -0,0 +1,19 @@
+//
+//  WorkView.swift
+//  Siroo
+//
+//  Created by jeongbae bang on 1/14/24.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text("TaskView")
+    }
+}
+
+#Preview {
+    TaskView()
+}
+
